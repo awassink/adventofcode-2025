641a0d163db250dd784131cb0116f7cc1d87a316
Arjen Wassink <awassink@quintor.nl>
2025-12-03 21:25:06 +0100

    Added day 2 - part 1 done.

--- day2/src/main/java/org/example/Day2.java
+++ day2/src/main/java/org/example/Day2.java
@@ -3,16 +3,66 @@ package org.example;
 import java.util.ArrayList;
 
 import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
 
 public class Day2 {
-    public static int[][] determineranges(String input) {
-        var result = new ArrayList<int[]>();
+    public static long[][] determineranges(String input) {
+        var result = new ArrayList<long[]>();
         var ranges = input.split(",");
         for (var range : ranges) {
             var values = range.split("-");
-            result.add(new int[]{parseInt(values[0]), parseInt(values[1])});
+            result.add(new long[]{parseLong(values[0]), parseLong(values[1])});
         }
-        var x = new int[result.size()][];
+        var x = new long[result.size()][];
         return result.toArray(x);
+    }
+    public static String[] invalidIDsP1(String input) {
+        var ranges = determineranges(input);
+        var result = new ArrayList<String>();
+        for (var range : ranges) {
+            for (var i = range[0]; i <= range[1]; i++) {
+                var s = String.valueOf(i);
+                if (s.length() % 2 == 0) {
+                    var s1 = s.substring(0, s.length() / 2);
+                    var s2 = s.substring(s.length() / 2);
+                    if (s1.equals(s2)) {
+                        result.add(s);
+//                        IO.println("Invalid ID: " + s);
+                    }
+                }
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+    public static long totalInvalidIDsP2(String input) {
+        var invalidIDs = invalidIDsP1(input);
+        long total = 0;
+        for (var invalidID : invalidIDs) {
+            total += parseLong(invalidID);
+        }
+        return total;
+    }
+    public static String[] invalidIDsP2(String input) {
+        var ranges = determineranges(input);
+        var result = new ArrayList<String>();
+        for (var range : ranges) {
+            for (var i = range[0]; i <= range[1]; i++) {
+                var s = String.valueOf(i);
+                for (var j = 1; j <= s.length()/2; j++){
+                    if (s.length() % j == 0) {
+                        String[] results = s.split("(?<=\\G.{" + j + "})");
+                        var same = true;
+                        for (var r : results) {
+                            same = same && results[0].equals(r);
+                        }
+                        if (same) {
+                            result.add(s);
+                            IO.println("Invalid ID: " + s);
+                        }
+                    }
+                }
+            }
+        }
+        return result.toArray(new String[0]);
     }
 }
--- day2/src/test/java/org/example/Day2Test.java
+++ day2/src/test/java/org/example/Day2Test.java
@@ -5,16 +5,48 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day2Test {
+
+    public static final String RANGES_INPUT = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
+            "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
+            "824824821-824824827,2121212118-2121212124";
+    public static final String FINAL_INPUT = "19391-47353,9354357-9434558,4646427538-4646497433,273-830,612658-674925,6639011-6699773," +
+            "4426384-4463095,527495356-527575097,22323258-22422396,412175-431622,492524-611114,77-122," +
+            "992964846-993029776,165081-338962,925961-994113,7967153617-7967231799,71518058-71542434," +
+            "64164836-64292066,4495586-4655083,2-17,432139-454960,4645-14066,6073872-6232058,9999984021-10000017929," +
+            "704216-909374,48425929-48543963,52767-94156,26-76,1252-3919,123-228";
+
     @Test
     void testDetermineranges() {
-        var rangesInput = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224," +
-                "1698522-1698528,446443-446449,38593856-38593862,565653-565659," +
-                "824824821-824824827,2121212118-2121212124";
-        var result = Day2.determineranges(rangesInput);
+        var result = Day2.determineranges(RANGES_INPUT);
         assertEquals(11,result.length);
         assertEquals(11,result[0][0]);
         assertEquals(22,result[0][1]);
         assertEquals(2121212118,result[10][0]);
         assertEquals(2121212124,result[10][1]);
+    }
+
+    @Test
+    void testInvalidIDsP1() {
+        var result = Day2.invalidIDsP1(RANGES_INPUT);
+        assertEquals("11",result[0]);
+        assertEquals("22",result[1]);
+        assertEquals("99",result[2]);
+        assertEquals("1010",result[3]);
+        assertEquals("1188511885",result[4]);
+        assertEquals("222222",result[5]);
+        assertEquals("446446",result[6]);
+        assertEquals("38593859",result[7]);
+    }
+
+    @Test
+    void testTotalInvalidIDsP2() {
+        var result = Day2.totalInvalidIDsP2(RANGES_INPUT);
+        assertEquals(1227775554,result);
+    }
+
+    @Test
+    void testTotalFinalInput() {
+        var result = Day2.totalInvalidIDsP2(FINAL_INPUT);
+        assertEquals(24747430309L,result);
     }
 }
